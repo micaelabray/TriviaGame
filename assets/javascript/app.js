ff2655b98016ffd26ad819ab82e27a2505c932ff
Micaela Bray <micaela.bray@gmail.com>
2019-06-01 15:09:32 -0500

    updates method in which questions are pulled

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,7 @@
+//variable to hold all of the questions and their answer options
 var questions = [
     {
+        number: "1",
         question: "On his first date with Robin, what instrument did Ted steal from the restaurant?",
         answers: [
             "Kazoo",
@@ -7,9 +9,10 @@ var questions = [
             "French Horn",
             "Triangle"
         ],
-        correctAnswer: "C"
+        correctAnswer: "French Horn"
     },
     {
+        number: "2",
         question: "What does GNB stand for?",
         answers: [
             "Global Networking Brand",
@@ -17,9 +20,10 @@ var questions = [
             "Goliath National Bank",
             "Grant Nutritional Brand",
         ],
-        correctAnswer: "C"
+        correctAnswer: "Goliath National Bank"
     },
     {
+        number: "3",
         question: "Who did Ted not date?",
         answers: [
             "Zoey",
@@ -27,9 +31,10 @@ var questions = [
             "Stella",
             "Nora",
         ],
-        correctAnswer: "D"
+        correctAnswer: "Nora"
     },
     {
+        number: "4",
         question: "Who is Robin's work enemy?",
         answer: [
             "Nora",
@@ -37,9 +42,10 @@ var questions = [
             "Stella",
             "Abby",
         ],
-        correctAnswer: "B"
+        correctAnswer: "Patrice"
     },
     {
+        number: "5",
         question: "How many slaps were awarded by Lily to Marshall during the Marshall-Barney Slap Bet?",
         answer: [
             "2",
@@ -51,36 +57,141 @@ var questions = [
     },
 ]
 
-var timer = 30;
-var correctAnswerTimer = 5;
-var resultsTimer = 5;
-var correctAnswer = 0;
-var incorrectAnswer = 0;
-// var results = 5;
+var timer;
+var correctAnswerTotal = 0;
+var incorrectAnswerTotal = 0;
+var questionDisplayed = 0;
+var answerSelection;
+var correctAnswer;
+var i;
+
 
 // Creating variables for each question number for when
-// going through the loop, the question is shown.
-var question1displayed = true;
-var question2displayed = false;
-var question3displayed = false;
-var question4displayed = false;
-var question5displayed = false;
+// // going through the loop, the question is shown.
+// var question1displayed = true;
+// var question2displayed = false;
+// var question3displayed = false;
+// var question4displayed = false;
+// var question5displayed = false;
+
+function pullQuestion () {
+    $("#questionNumber").html("Question number " + questions[questionDisplayed].number + " of 5");
+    $("#question").html(questions[questionDisplayed].question);
+    $("#answerA").html("A: " + questions[questionDisplayed].answers[0]);
+    $("#answerB").html("B: " + questions[questionDisplayed].answers[1]);
+    $("#answerC").html("C: " + questions[questionDisplayed].answers[2]);
+    $("#answerD").html("D: " + questions[questionDisplayed].answers[3]);
+}
+
+//Countdown for seconds remaining for question
+function timer() {
+    i = 10;
+    
+   timer = setInterval (function(){
+       i--;
+       $("#timer").html("Time Left: " + i);
+    
+       if (i === 0) {
+           clearInterval(timer);
+           $("#timer").html("Time is up. The correct answer was " + questions[questionDisplayed].correctAnswer + ".");
+           incorrectAnswerTotal++;
+           $("#incorrect").html(incorrectAnswerTotal);
+       }
+   }, 1000);
+}
+
 
 $(document).ready(function(){
-    // Countdown for seconds remaining for question
-    function timer() {
-        $("#timer").html("Time Left: 30");
-        timer = setInterval (endOfTimer(), 1000);
-    }
+
+    // Starts the game running
+    $("#question").click(function () {
+        i = 0;
+        pullQuestion();
+        timer();
+        // checkAnswer ();
+    });
+
+    //Checks to see if selected answer matches correct answer
+    $("#answerA").click(function(){
+        clearInterval(timer);
+        if (questions[questionDisplayed].answers[0] === questions[questionDisplayed].correctAnswer) {
+            correctAnswerTotal++;
+            $("#correct").html(correctAnswerTotal);
+            $("#timer").html("Correct, the answer was " + questions[questionDisplayed].correctAnswer);
+        }
+        else {
+            incorrectAnswerTotal++;
+            $("#incorrect").html(incorrectAnswerTotal);
+            $("#timer").html("Incorrect, the answer was " + questions[questionDisplayed].correctAnswer);
+        }
+        setTimeout(function(){
+            questionDisplayed++;
+            pullQuestion ();
+        }, 3000);
+    })
+
+    $("#answerB").click(function(){
+        clearInterval(timer);
+        if (questions[questionDisplayed].answers[1] === questions[questionDisplayed].correctAnswer) {
+            correctAnswerTotal++;
+            $("#correct").html(correctAnswerTotal);
+            $("#timer").html("Correct, the answer was " + questions[questionDisplayed].correctAnswer);
+        }
+        else {
+            incorrectAnswerTotal++;
+            $("#incorrect").html(incorrectAnswerTotal);
+            $("#timer").html("Incorrect, the answer was " + questions[questionDisplayed].correctAnswer);
+        }
+        setTimeout(function(){
+            questionDisplayed++;
+            pullQuestion ();
+        }, 3000);
+    })
+
+    $("#answerC").click(function(){
+        clearInterval(timer);
+        if (questions[questionDisplayed].answers[2] === questions[questionDisplayed].correctAnswer) {
+            correctAnswerTotal++;
+            $("#correct").html(correctAnswerTotal);
+            $("#timer").html("Correct, the answer was " + questions[questionDisplayed].correctAnswer);
+        }
+        else {
+            incorrectAnswerTotal++;
+            $("#incorrect").html(incorrectAnswerTotal);
+            $("#timer").html("Incorrect, the answer was " + questions[questionDisplayed].correctAnswer);
+        }
+        setTimeout(function(){
+            questionDisplayed++;
+            pullQuestion ();
+        }, 3000);
+    })
+
+    $("#answerD").click(function(){
+        clearInterval(timer);
+        if (questions[questionDisplayed].answers[3] === questions[questionDisplayed].correctAnswer) {
+            correctAnswerTotal++;
+            $("#correct").html(correctAnswerTotal);
+            $("#timer").html("Correct, the answer was " + questions[questionDisplayed].correctAnswer);
+        }
+        else {
+            incorrectAnswerTotal++;
+            $("#incorrect").html(incorrectAnswerTotal);
+            $("#timer").html("Incorrect, the answer was " + questions[questionDisplayed].correctAnswer);
+        }
+        setTimeout(function(){
+            questionDisplayed++;
+            pullQuestion ();
+        }, 3000);
+    })
 
 
     // function to show question
 
-    function endOfTimer () {
-        clearInterval(timer);
-        clearInterval(correctAnswerTimer);
-        clearInterval(resultsTimer);
-        $("#timer").html("Time is up.");
+    // function endOfTimer () {
+    //     clearInterval(timer);
+    //     clearInterval(correctAnswerTimer);
+    //     clearInterval(resultsTimer);
+    //     $("#timer").html("Time is up.");
 
         //shows Question 2
         // if (question1displayed==true){
@@ -113,48 +224,48 @@ $(document).ready(function(){
         //     timer = 30;
         //     question5 ();
         // }
-    }
+    // }
 
     // // Question1
-    function question1 () {
-        $("#answerDisplay").html("");
-        $("#questionNumber").html("Question 1 of 5");
-        $("#question").html("On his first date with Robin, what instrument did Ted steal from the restaurant?");
+    // function question1 () {
+    //     $("#answerDisplay").html("");
+    //     $("#questionNumber").html("Question 1 of 5");
+    //     $("#question").html("On his first date with Robin, what instrument did Ted steal from the restaurant?");
 
-        //Answers for the question
-        $("#answerA").html("Kazoo");
-        $("#answerB").html("Clarinet");
-        $("#answerC").html("French Horn");
-        $("#answerD").html("Triangle");
+    //     //Answers for the question
+    //     $("#answerA").html("Kazoo");
+    //     $("#answerB").html("Clarinet");
+    //     $("#answerC").html("French Horn");
+    //     $("#answerD").html("Triangle");
 
-        $("#answerA").on("click", question1AnswerA);
-        $("#answerB").on("click", question1AnswerB);
-        $("#answerC").on("click", question1AnswerC);
-        $("#answerD").on("click", question1AnswerD);        
-    }
+    //     $("#answerA").on("click", question1AnswerA);
+    //     $("#answerB").on("click", question1AnswerB);
+    //     $("#answerC").on("click", question1AnswerC);
+    //     $("#answerD").on("click", question1AnswerD);        
+    // }
 
     // console.log(question1);
 
     // // // Question2
-    function question2 () {
-        $("#answerDisplay").html("");
-        $("#questionNumber").html("Question 2 of 5");
-        $("#question").html("What does GNB stand for?");
+    // function question2 () {
+    //     $("#answerDisplay").html("");
+    //     $("#questionNumber").html("Question 2 of 5");
+    //     $("#question").html("What does GNB stand for?");
 
-        //Answers for the question
-        $("#answerA").html("Global Networking Brand");
-        $("#answerB").html("Greenway National Bank");
-        $("#answerC").html("Goliath National Bank");
-        $("#answerD").html("Grant Nutritional Brand");
+    //     //Answers for the question
+    //     $("#answerA").html("Global Networking Brand");
+    //     $("#answerB").html("Greenway National Bank");
+    //     $("#answerC").html("Goliath National Bank");
+    //     $("#answerD").html("Grant Nutritional Brand");
 
-        $("#answerA").on("click", question2AnswerA);
-        $("#answerB").on("click", question2AnswerB);
-        $("#answerC").on("click", question2AnswerC);
-        $("#answerD").on("click", question2AnswerD);
+    //     $("#answerA").on("click", question2AnswerA);
+    //     $("#answerB").on("click", question2AnswerB);
+    //     $("#answerC").on("click", question2AnswerC);
+    //     $("#answerD").on("click", question2AnswerD);
 
-        // Adds timer to question
-        // timer();
-    }
+    //     // Adds timer to question
+    //     // timer();
+    // }
 
     // // Question3
     // function question3 () {
@@ -222,59 +333,58 @@ $(document).ready(function(){
 
 
     //functions for answers for question 1
-    function question1AnswerA(){
-        incorrectAnswer++;
-        // clearInterval(Timer)
-        $("#timer").html("Time is up.");
-        $("#answerA").html("");
-        $("#answerB").html("");
-        $("#answerC").html("");
-        $("#answerD").html("");
-        $(".answerDisplay").html("Oops, the correct answer was French Horn.");
-        clearInterval(resultsTimer);
-        question2();
-    }
+    // function question1AnswerA(){
+    //     incorrectAnswer++;
+    //     // clearInterval(Timer)
+    //     $("#timer").html("Time is up.");
+    //     $("#answerA").html("");
+    //     $("#answerB").html("");
+    //     $("#answerC").html("");
+    //     $("#answerD").html("");
+    //     $(".answerDisplay").html("Oops, the correct answer was French Horn.");
+    //     clearInterval(resultsTimer);
+    //     question2();
+    // }
 
-    function question1AnswerB(){
-        incorrectAnswer++;
-        // clearInterval(Timer)
-        $("#timer").html("Time is up.");
-        $("#answerA").html("");
-        $("#answerB").html("");
-        $("#answerC").html("");
-        $("#answerD").html("");
-        $(".answerDisplay").html("Oops, the correct answer was French Horn.");
-        clearInterval(resultsTimer);
-        question2();
-    }
+    // function question1AnswerB(){
+    //     incorrectAnswer++;
+    //     // clearInterval(Timer)
+    //     $("#timer").html("Time is up.");
+    //     $("#answerA").html("");
+    //     $("#answerB").html("");
+    //     $("#answerC").html("");
+    //     $("#answerD").html("");
+    //     $(".answerDisplay").html("Oops, the correct answer was French Horn.");
+    //     clearInterval(resultsTimer);
+    //     question2();
+    // }
 
-    function question1AnswerC (){
-        correctAnswer++;
-        // clearInterval(Timer)
-        $("#timer").html("Time is up.");
-        $("#answerA").html("");
-        $("#answerB").html("");
-        $("#answerC").html("");
-        $("#answerD").html("");
-        $(".answerDisplay").html("Correct")
-        clearInterval(resultsTimer);
-        question2();
-    }
+    // function question1AnswerC (){
+    //     correctAnswer++;
+    //     // clearInterval(Timer)
+    //     $("#timer").html("Time is up.");
+    //     $("#answerA").html("");
+    //     $("#answerB").html("");
+    //     $("#answerC").html("");
+    //     $("#answerD").html("");
+    //     $(".answerDisplay").html("Correct")
+    //     clearInterval(resultsTimer);
+    //     question2();
+    // }
 
-    function question1AnswerD(){
-        incorrectAnswer++;
-        // clearInterval(Timer)
-        $("#timer").html("Time is up.");
-        $("#answerA").html("");
-        $("#answerB").html("");
-        $("#answerC").html("");
-        $("#answerD").html("");
-        $(".answerDisplay").html("Oops, the correct answer was French Horn.");
-        clearInterval(resultsTimer);
-        question2();
-    }
-
-
-    question1();
+    // function question1AnswerD(){
+    //     incorrectAnswer++;
+    //     // clearInterval(Timer)
+    //     $("#timer").html("Time is up.");
+    //     $("#answerA").html("");
+    //     $("#answerB").html("");
+    //     $("#answerC").html("");
+    //     $("#answerD").html("");
+    //     $(".answerDisplay").html("Oops, the correct answer was French Horn.");
+    //     clearInterval(resultsTimer);
+    //     question2();
+    // }
 
 });
+
+    // question1();
